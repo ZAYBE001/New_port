32c6eef430471de41f905d4d5fb352be49ea4787
zaybe <zaybe.maliq@student.moringaschool.com>
2026-06-15 23:06:28 +0300

    final live up

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import Grainient from './components/background.jsx'; 
+import myPortrait from './assets/me.jpeg';
 
 function App() {
   // 1. React State Management
